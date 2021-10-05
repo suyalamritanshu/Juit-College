a65ca1ba6922b55f25e01e0e9697f9a6303a7211
Siddharth Singh <siddhartsingh2002@gmail.com>
2021-10-05 10:19:58 +0530

    Add splash screen

---
+++ JuitCollege/app/src/main/java/com/example/juitcollege/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.juitcollege;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+
+    }
+}
